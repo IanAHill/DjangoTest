b649dbb34b395de72d312090e12870b5b25691af
Ian <ian@mag.industries>
2023-01-11 11:46:00 -0500

    added python anywhere to allowed hosts in settings py

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^(l!db9-sz@ash-mkic_wv0=+!eq-=o120j769t(a3nthyqz4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ianahill.pythonanywhere.com']
 
 
 # Application definition
